d07aabc06f6c683765a0d0f3c93675d85216161b
SeinaTajika <92138597+SeinaTajika@users.noreply.github.com>
2022-10-27 08:38:26 +0900

    おみくじ

--- bolt.js
+++ bolt.js
@@ -1,17 +1,19 @@
 'use strict';
-const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const bolt = require('@slack/bolt')
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = new bolt.App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-  logLevel: 'debug'
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    logLevel: 'debug'
+})
+
+app.message(/hello/i,({message,say})=>{
+    say(`こんにちは！ <@${message.user}>さん`);
 });
 
-app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
-});
+
 
 app.start();
